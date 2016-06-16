50e3be43514a420f5b703ff8ec574ea3d74dfa72
Zhaoge <zhaoge@oradt.com>
2016-06-16 17:25:15 +0800

    [code]布局 弹框

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="/Public/css/main.css">
     <script src="/Public/js/jquery-2.1.1.min.js"></script>
     <script src="/Public/js/bootstrap.min.js"></script>
+    <script src="/Public/js/jquery.mobile-1.4.5.min.js"></script>
+
     <meta charset="UTF-8">
     <!--缩放-->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
@@ -23,50 +25,50 @@
 <div id="main_body" class="container-fluid">
     
 <div class="row">
-    <div class="col-xs-4 col-sm-5 col-md-3 col-lg-2 myphoto" >
-        <div class="myphoto_wrap">
+    <div class="col-xs-4 col-sm-5 col-md-3 col-lg-2 mainIco" windowsName="photo" >
+        <div class="mainIco_wrap">
         <span class="glyphicon glyphicon-picture" aria-hidden="true"></span>
         </div>
         <span>相册</span>
     </div>
 
-    <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2  myphoto" style="background-color: rgb(178,26,65)">
-            <div class="myphoto_wrap">
+    <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2  mainIco" style="background-color: rgb(178,26,65)">
+            <div class="mainIco_wrap">
                 <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
             </div>
             <span>文件夹</span>
     </div>
 
-    <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 myphoto" style="background-color: rgb(210,71,38)">
-        <div class="myphoto_wrap">
+    <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2 mainIco" style="background-color: rgb(210,71,38)">
+        <div class="mainIco_wrap">
             <span class="glyphicon glyphicon-headphones" aria-hidden="true"></span>
         </div>
         <span>音乐</span>
     </div>
 
-    <div class="col-xs-3 col-sm-4 col-md-3 col-lg-2 myphoto" style="background-color: rgb(255,190,0)">
-        <div class="myphoto_wrap">
+    <div class="col-xs-3 col-sm-4 col-md-3 col-lg-2 mainIco" style="background-color: rgb(255,190,0)">
+        <div class="mainIco_wrap">
             <span class="glyphicon glyphicon-film" aria-hidden="true"></span>
         </div>
         <span>视频</span>
     </div>
 
-    <div class="col-xs-3 col-sm-4 col-md-3 col-lg-2 myphoto" style="background-color: rgb(81,51,171)">
-        <div class="myphoto_wrap">
+    <div class="col-xs-3 col-sm-4 col-md-3 col-lg-2 mainIco" style="background-color: rgb(81,51,171)">
+        <div class="mainIco_wrap">
             <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
         </div>
         <span>设置</span>
     </div>
 
-    <div class="col-xs-2 col-sm-4 col-md-3 col-lg-2 myphoto" style="background-color: rgb(0,138,0)">
-        <div class="myphoto_wrap">
+    <div class="col-xs-2 col-sm-4 col-md-3 col-lg-2 mainIco" style="background-color: rgb(0,138,0)">
+        <div class="mainIco_wrap">
             <span class="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span>
         </div>
         <span>上传</span>
     </div>
 
-    <div class="col-xs-3 col-sm-4 col-md-3 col-lg-2 myphoto" style="background-color: rgb(159,0,166)">
-        <div class="myphoto_wrap">
+    <div class="col-xs-3 col-sm-4 col-md-3 col-lg-2 mainIco" style="background-color: rgb(159,0,166)">
+        <div class="mainIco_wrap">
             <span class="glyphicon glyphicon-send" aria-hidden="true"></span>
         </div>
         <span>注册</span>
@@ -77,13 +79,13 @@
 
 
 
-    <!--弹出窗口-->
-<div class="panel panel-default" id="windows_wrap">
-<div class="panel-heading" id="windows_ico">
+    <!--相册弹出窗口-->
+<div class="panel panel-default  windows_wrap" windowsName="photo">
+<div class="panel-heading  windows_ico">
     标题
-    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-    <span class="glyphicon glyphicon-modal-window" aria-hidden="true"></span>
-    <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+    <span class="glyphicon glyphicon-remove close_window " aria-hidden="true"></span>
+    <span class="glyphicon glyphicon-modal-window mid_window" aria-hidden="true"></span>
+    <span class="glyphicon glyphicon-minus small_window" aria-hidden="true"></span>
 </div>
 <div class="panel-body">
     人生的成败往往就在一念之差,失败者并不比成功者逊色,只是他们缺少了斗志.那叫做奋斗.
@@ -106,21 +108,7 @@
     　　成功从来不是偶然,中国上下五千年没有一个伟人是巧合成功,他们有奋斗的证据,他们的成长足迹中有付出过心血,他们一路走过来的脚印中,没有完完整整的,有膝盖的磕印,有摔倒的血迹,有汗水浸湿的泥沙.他们从哪里摔倒就从哪里爬起来,生命的卑微因奋斗而变得精彩.奋斗会伴随着我们一生,但奋斗中的艰辛并不会持续着,历经艰辛,后面的路会渐渐平坦.
     　　我们奋斗的目标也许不一样,但动力源泉是一样的.有些人奋斗着为了考上好的学校；有些人奋斗着找到好的工作职位；有些人奋斗着为国家做出贡献······有些人一生在奋斗.奋斗,有辛酸的泪水,有误解,有难舍难分之处,但这一切,无法阻挠我们的前进.
     　　奋斗过,才会懂,会坚强,会前进.
-    人生的成败往往就在一念之差,失败者并不比成功者逊色,只是他们缺少了斗志.那叫做奋斗.
-    　　上帝是公平的,他给予我们每个人奋斗,只是有些人没有彻悟,没有好好利用.奋斗也是一种生命物质,会随着经历挫折而越长越大,越动荡,越勇敢,奋斗在推动我们前进.俗话说,逆境出人才,逆境中奋斗才会爆发,才会不停地在逆境中寻找生的希望.黑暗的深渊中,奋斗是微光；干涸的土地中,奋斗是水滴；严寒的肃冬中,奋斗是热量.
-    　　成功从来不是偶然,中国上下五千年没有一个伟人是巧合成功,他们有奋斗的证据,他们的成长足迹中有付出过心血,他们一路走过来的脚印中,没有完完整整的,有膝盖的磕印,有摔倒的血迹,有汗水浸湿的泥沙.他们从哪里摔倒就从哪里爬起来,生命的卑微因奋斗而变得精彩.奋斗会伴随着我们一生,但奋斗中的艰辛并不会持续着,历经艰辛,后面的路会渐渐平坦.
-    　　我们奋斗的目标也许不一样,但动力源泉是一样的.有些人奋斗着为了考上好的学校；有些人奋斗着找到好的工作职位；有些人奋斗着为国家做出贡献······有些人一生在奋斗.奋斗,有辛酸的泪水,有误解,有难舍难分之处,但这一切,无法阻挠我们的前进.
-    　　奋斗过,才会懂,会坚强,会前进.
-    人生的成败往往就在一念之差,失败者并不比成功者逊色,只是他们缺少了斗志.那叫做奋斗.
-    　　上帝是公平的,他给予我们每个人奋斗,只是有些人没有彻悟,没有好好利用.奋斗也是一种生命物质,会随着经历挫折而越长越大,越动荡,越勇敢,奋斗在推动我们前进.俗话说,逆境出人才,逆境中奋斗才会爆发,才会不停地在逆境中寻找生的希望.黑暗的深渊中,奋斗是微光；干涸的土地中,奋斗是水滴；严寒的肃冬中,奋斗是热量.
-    　　成功从来不是偶然,中国上下五千年没有一个伟人是巧合成功,他们有奋斗的证据,他们的成长足迹中有付出过心血,他们一路走过来的脚印中,没有完完整整的,有膝盖的磕印,有摔倒的血迹,有汗水浸湿的泥沙.他们从哪里摔倒就从哪里爬起来,生命的卑微因奋斗而变得精彩.奋斗会伴随着我们一生,但奋斗中的艰辛并不会持续着,历经艰辛,后面的路会渐渐平坦.
-    　　我们奋斗的目标也许不一样,但动力源泉是一样的.有些人奋斗着为了考上好的学校；有些人奋斗着找到好的工作职位；有些人奋斗着为国家做出贡献······有些人一生在奋斗.奋斗,有辛酸的泪水,有误解,有难舍难分之处,但这一切,无法阻挠我们的前进.
-    　　奋斗过,才会懂,会坚强,会前进.
-    人生的成败往往就在一念之差,失败者并不比成功者逊色,只是他们缺少了斗志.那叫做奋斗.
-    　　上帝是公平的,他给予我们每个人奋斗,只是有些人没有彻悟,没有好好利用.奋斗也是一种生命物质,会随着经历挫折而越长越大,越动荡,越勇敢,奋斗在推动我们前进.俗话说,逆境出人才,逆境中奋斗才会爆发,才会不停地在逆境中寻找生的希望.黑暗的深渊中,奋斗是微光；干涸的土地中,奋斗是水滴；严寒的肃冬中,奋斗是热量.
-    　　成功从来不是偶然,中国上下五千年没有一个伟人是巧合成功,他们有奋斗的证据,他们的成长足迹中有付出过心血,他们一路走过来的脚印中,没有完完整整的,有膝盖的磕印,有摔倒的血迹,有汗水浸湿的泥沙.他们从哪里摔倒就从哪里爬起来,生命的卑微因奋斗而变得精彩.奋斗会伴随着我们一生,但奋斗中的艰辛并不会持续着,历经艰辛,后面的路会渐渐平坦.
-    　　我们奋斗的目标也许不一样,但动力源泉是一样的.有些人奋斗着为了考上好的学校；有些人奋斗着找到好的工作职位；有些人奋斗着为国家做出贡献······有些人一生在奋斗.奋斗,有辛酸的泪水,有误解,有难舍难分之处,但这一切,无法阻挠我们的前进.
-    　　奋斗过,才会懂,会坚强,会前进.
+
 </div>
 </div>
 </div>
@@ -136,25 +124,25 @@
         <!--任务栏窗口-->
         <div   class="col-xs-7 col-sm-8 col-md-9 col-lg-9 ">
             <div id="windows_btn_wrap">
-                <button type="button"  class="btn btn-primary btn-1g windows_btn"  >
+                <button type="button"  class="btn btn-primary btn-1g windows_btn" windowsName="photo" >
                     <span class="glyphicon glyphicon-picture " aria-hidden="true" ></span>相册
                 </button>
-                <button type="button"  class="btn btn-primary btn-1g windows_btn">
+                <button type="button"  class="btn btn-primary btn-1g windows_btn"windowsName="file">
                     <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>文件夹
                 </button>
-                <button type="button"  class="btn btn-primary btn-1g windows_btn">
+                <button type="button"  class="btn btn-primary btn-1g windows_btn" windowsName="mucic">
                     <span class="glyphicon glyphicon-headphones" aria-hidden="true"></span>音乐
                 </button>
-                <button type="button"  class="btn btn-primary btn-1g windows_btn">
+                <button type="button"  class="btn btn-primary btn-1g windows_btn" windowsName="ridio">
                     <span class="glyphicon glyphicon-film" aria-hidden="true"></span>视频
                 </button>
-                <button type="button"  class="btn btn-primary btn-1g windows_btn">
+                <button type="button"  class="btn btn-primary btn-1g windows_btn" windowsName="config">
                     <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>设置
                 </button>
-                <button type="button"  class="btn btn-primary btn-1g windows_btn">
+                <button type="button"  class="btn btn-primary btn-1g windows_btn"windowsName="upload">
                     <span class="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span>上传
                 </button>
-                <button type="button"  class="btn btn-primary btn-1g windows_btn">
+                <button type="button"  class="btn btn-primary btn-1g windows_btn" windowsName="login">
                     <span class="glyphicon glyphicon-send" aria-hidden="true"></span>注册
                 </button>
              </div>
@@ -174,6 +162,7 @@
 </div>
 
 </footer>
+<script src="/Public/js/main.js"></script>
 <!--引入窗口弹框-->
 
 </body>
--- Public/js/main.js
+++ Public/js/main.js
@@ -1,4 +1,92 @@
 /**
  * Created by zhaoge on 2016/6/15.
+ * 主体js
  */
 
+(function($){
+    $.extend({
+        main:{
+            init:function(){
+                this.mainTap();
+                this.footWindowsTap();
+                this.windowsIcoTap();
+
+
+            },
+            /*主菜单tap事件*/
+            mainTap:function(){
+                $(".mainIco").on("tap",function(){
+                    var winName=$(this).attr('windowsName');//获取与底部窗口对应的name
+                    $(".windows_wrap[windowsName="+winName+"]").show(300);
+                    var windows_btn_obj=$(".windows_btn[windowsName="+winName+"]");//获取对应的底部窗口
+                    windows_btn_obj.show(300);//底部窗口显示
+                    windows_btn_obj.addClass('btn-warning');//底部窗口背景色变色（按钮增加class）
+                    return false;
+                });
+
+            },
+
+            /*底部窗口*/
+           footWindowsTap:function(){
+               $('.windows_btn').on("tap",function(){
+                   var winName=$(this).attr('windowsName');//获取底部窗口name
+                   var windows_wrap_obj=  $(".windows_wrap[windowsName="+winName+"]");//获取name对应的弹窗对象
+                   if(windows_wrap_obj.css('display')=='none'){ //判断弹窗状态 点击显示和隐藏 自身背景色变化
+                       windows_wrap_obj.show(300);
+                       $(this).addClass('btn-warning')
+                   }else{
+                       windows_wrap_obj.hide(200);
+                       $(this).removeClass('btn-warning')
+                   }
+                   return false;
+
+               })
+
+           },
+            /*弹出框头部图标点击*/
+          windowsIcoTap:function(){
+              /*窗口最小化按钮点击事件*/
+              $(".small_window").on('tap',function(){
+                  var thisWin=$(this).parents(".windows_wrap"); //获取自身整个窗口
+                  var winName=thisWin.attr('windowsName'); //获取窗口name
+                  var this_window_btn=  $(".windows_btn[windowsName="+winName+"]"); //获取对应的底部窗口
+                  thisWin.hide(200);//点击隐藏模拟最小化
+                  this_window_btn.removeClass('btn-warning');//底部窗口背景色改变
+
+              });
+
+              /*窗口放大缩小窗口事件*/
+              $(".mid_window").on('tap',function(){
+                  var thisWin=$(this).parents(".windows_wrap"); //获取自身整个窗口
+                  var thisWinHead=$(this).parents(".windows_ico");
+                  var winWidth=thisWin.css('width');
+                  var maxWidth=$(window).width()+"px";
+                  if(winWidth==maxWidth){
+                      thisWin.css({"height":"60%","width":"60%","left":"20%","top":"20%"});
+                      thisWinHead.css({"width":"60%","left":"20%","top":"20%"});
+
+                  }else{
+                      thisWin.css({"height":"90%","width":"100%","left":"0","top":"0"});
+                      thisWinHead.css({"width":"100%","left":"0","top":"0"});
+
+                  }
+
+              });
+
+
+
+          }
+
+
+        }
+
+    });
+    $.main.init();
+})(jQuery);
+
+/*
+$(function(){
+    $('body').on("tap",function(){
+        alert(123);
+    })
+});*/
